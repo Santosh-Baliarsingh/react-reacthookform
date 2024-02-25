8fa06435bad59ae23691bd28eb35f149ae4118b3
santosh Baliarsingh <santoshbaliarsingh1997@gmail.com>
2024-02-26 00:55:37 +0530

    UI Updated

--- src/components/ValidationWithMUI.tsx
+++ src/components/ValidationWithMUI.tsx
@@ -1,6 +1,8 @@
 import { Box, Button, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
+import { styles } from "./ValidationWithMui.style";
+import defaultImg from "../assets/defaultImage.jpg";
 
 interface FormData {
   firstName: string;
@@ -10,6 +12,7 @@ interface FormData {
   image: string;
 }
 
+const formData = localStorage.getItem("formData");
 const ValidationWithMUI = () => {
   const {
     register,
@@ -18,23 +21,9 @@ const ValidationWithMUI = () => {
     trigger,
     formState: { errors },
   } = useForm<FormData>();
-  const [errorMessages, setErrorMessages] = useState<string[]>([]);
   const [image, setImage] = useState<string | ArrayBuffer | null>(null);
   const [formDataArray, setFormDataArray] = useState<FormData[]>([]);
-
-  const handleBlur = async () => {
-    const result = await trigger("password");
-    const errorMessage = await errors.password?.message;
-    if (result) {
-      setErrorMessages((prevState) =>
-        prevState.filter((msg) => msg !== errorMessage)
-      );
-    } else {
-      if (errorMessage) {
-        setErrorMessages((prevState) => [...prevState, errorMessage]);
-      }
-    }
-  };
+  const [imageUrl, setImageUrl] = useState<string>(defaultImg);
 
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files && event.target.files[0];
@@ -42,6 +31,7 @@ const ValidationWithMUI = () => {
       const reader = new FileReader();
       reader.onloadend = () => {
         setImage(reader.result);
+        setImageUrl(reader.result as string);
       };
       reader.readAsDataURL(file);
     }
@@ -51,7 +41,7 @@ const ValidationWithMUI = () => {
     console.table(data);
     const existingData = localStorage.getItem("formData");
     const formDataArray = existingData ? JSON.parse(existingData) : [];
-    data.image = image as string; 
+    data.image = image as string;
     formDataArray.push(data);
     localStorage.setItem("formData", JSON.stringify(formDataArray));
     reset();
@@ -66,117 +56,146 @@ const ValidationWithMUI = () => {
   }, []);
 
   return (
-    <Box
-      component="form"
-      onSubmit={handleSubmit(onSubmit)}
-      noValidate
-      autoComplete="off"
-      sx={{ margin: "10px 0" }}
-    >
-      <input type="file" onChange={handleImageUpload} />
-      <TextField
-        label="First Name"
-        {...register("firstName", {
-          required: "FirstName is required!",
-          minLength: {
-            value: 5,
-            message: "FirstName should be at least 5 characters!",
-          },
-          maxLength: {
-            value: 10,
-            message: 'FirstName should not exceed 10 characters!",',
-          },
-        })}
-        onBlur={() => {
-          trigger("firstName");
-        }}
-        error={Boolean(errors.firstName)}
-        helperText={errors.firstName?.message}
-      />
-      <TextField
-        label="Last Name"
-        {...register("lastName", {
-          required: "LastName is required!",
-          minLength: {
-            value: 5,
-            message: "LastName should be at least 5 characters!",
-          },
-          maxLength: {
-            value: 15,
-            message: 'LastName should not exceed 10 characters!",',
-          },
-        })}
-        onBlur={() => {
-          trigger("lastName");
-        }}
-        error={Boolean(errors.lastName)}
-        helperText={errors.lastName?.message}
-      />
-      <TextField
-        label="Email"
-        type="email"
-        {...register("email", {
-          required: {
-            value: true,
-            message: "Email is required!",
-          },
-          pattern: {
-            value:
-              /^(([^<>()/[\]\\.,;:\s@"]+(\.[^<>()/[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-            message: "Invalid Email Address",
-          },
-        })}
-        onBlur={() => {
-          trigger("email");
-        }}
-        error={Boolean(errors.email)}
-        helperText={errors.email?.message}
-      />
-      <TextField
-        label="Password"
-        type="password"
-        {...register("password", {
-          required: {
-            value: true,
-            message: "Password is required!",
-          },
-          pattern: {
-            value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}$/,
-            message:
-              "Password must contain at least one uppercase letter, one lowercase letter, and one number, and must be between 6 and 15 characters long",
-          },
-        })}
-        onBlur={handleBlur}
-        error={Boolean(errors.password)}
-        helperText={errors.password?.message}
-      />
-      <ol>
-        {errorMessages.map((message, index) => (
-          <li key={index} style={{ color: errors.password ? "red" : "green" }}>
-            {message}
-          </li>
-        ))}
-      </ol>
-      <Button type="submit" variant="contained" color="primary">
-        Submit
-      </Button>
-      <Box>
-        <Box>Login Details</Box>
-        {formDataArray.map((data, index) => (
-            <Box key={index}>
-              <Box
-                component="img"
-                sx={{ width: "200px", height : '200px', borderRadius: "50%" }}
-                src={data.image}
-                alt="Uploaded"
-              />
-            <Box>First Name: {data.firstName}</Box>
-            <Box>Last Name: {data.lastName}</Box>
-            <Box>Email: {data.email}</Box>
-            <Box>Password: {data.password}</Box>
+    <Box sx={styles.container}>
+      <Box sx={styles.formContainer}>
+        <Box
+          component="form"
+          onSubmit={handleSubmit(onSubmit)}
+          noValidate
+          autoComplete="off"
+          sx={styles.formBox}
+        >
+          <Box
+            component="input"
+            id="fileInput"
+            display="none"
+            type="file"
+            onChange={handleImageUpload}
+          />
+          <Box
+            component="img"
+            src={imageUrl}
+            sx={styles.defaultImg}
+            alt="Click to select an image"
+            onClick={() => document.getElementById("fileInput")?.click()}
+            style={{ cursor: "pointer" }}
+          />
+          <TextField
+            label="First Name"
+            sx={styles.textfield}
+            {...register("firstName", {
+              required: "FirstName is required!",
+              minLength: {
+                value: 5,
+                message: "FirstName should be at least 5 characters!",
+              },
+              maxLength: {
+                value: 10,
+                message: 'FirstName should not exceed 10 characters!",',
+              },
+            })}
+            onBlur={() => {
+              trigger("firstName");
+            }}
+            error={Boolean(errors.firstName)}
+            helperText={errors.firstName?.message}
+          />
+          <TextField
+            label="Last Name"
+            sx={styles.textfield}
+            {...register("lastName", {
+              required: "LastName is required!",
+              minLength: {
+                value: 5,
+                message: "LastName should be at least 5 characters!",
+              },
+              maxLength: {
+                value: 15,
+                message: 'LastName should not exceed 10 characters!",',
+              },
+            })}
+            onBlur={() => {
+              trigger("lastName");
+            }}
+            error={Boolean(errors.lastName)}
+            helperText={errors.lastName?.message}
+          />
+          <TextField
+            label="Email"
+            sx={styles.textfield}
+            type="email"
+            {...register("email", {
+              required: {
+                value: true,
+                message: "Email is required!",
+              },
+              pattern: {
+                value:
+                  /^(([^<>()/[\]\\.,;:\s@"]+(\.[^<>()/[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                message: "Invalid Email Address",
+              },
+            })}
+            onBlur={() => {
+              trigger("email");
+            }}
+            error={Boolean(errors.email)}
+            helperText={errors.email?.message}
+          />
+          <Box>
+            <TextField
+              label="Password"
+              sx={styles.textfield}
+              type="password"
+              {...register("password", {
+                required: {
+                  value: true,
+                  message: "Password is required!",
+                },
+                pattern: {
+                  value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}$/,
+                  message:
+                    "Password must contain at least one uppercase letter, one lowercase letter and one number, and must be between 6 and 15 characters long.",
+                },
+              })}
+              onBlur={() => trigger("password")}
+              error={Boolean(errors.password)}
+              // helperText={errors.password?.message}
+            />
+            <Box sx={styles.passwordstyle}>{errors.password?.message}</Box>
           </Box>
-        ))}
+
+          <Button type="submit" variant="contained" color="primary">
+            Submit
+          </Button>
+        </Box>
       </Box>
+      {formData && (
+        <Box sx={styles.cardContainer}>
+          <Box sx={styles.heading}>User Details</Box>
+          <Box sx={styles.logindetailscontainer}>
+            <Box sx={styles.cardbox}>
+              {formDataArray.map((data, index) => (
+                <Box sx={styles.card} key={index}>
+                  <Box
+                    component="img"
+                    sx={{
+                      width: "200px",
+                      height: "200px",
+                      borderRadius: "50%",
+                    }}
+                    src={data.image}
+                    alt="Uploaded"
+                  />
+                  <Box>First Name: {data.firstName}</Box>
+                  <Box>Last Name: {data.lastName}</Box>
+                  <Box>Email: {data.email}</Box>
+                  <Box>Password: {data.password}</Box>
+                </Box>
+              ))}
+            </Box>
+          </Box>
+        </Box>
+      )}
     </Box>
   );
 };
---
+++ src/components/ValidationWithMui.style.tsx
@@ -0,0 +1,65 @@
+export const styles = {
+  container: {
+    width: "100%",
+  },
+  formContainer: {
+    display: "flex",
+    justifyContent: "center",
+    margin: "20px 0",
+  },
+  formBox: {
+    borderRadius: "20px",
+    alignItems: "center",
+    width: {xs : '80%',md :"85%"},
+    display: "flex",
+    flexDirection : {xs :'column' , md : 'row'},
+    padding: "50px",
+    boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 12px",
+  },
+  defaultImg: {
+    width: "80px",
+    height: "80px",
+    borderRadius: "50px",
+    margin: "0 10px",
+  },
+  textfield: {
+    width: {xs :"220px" , sm : '400px' , md : '220px'},
+    margin: {xs : '10px 5px',md :"0 10px"},
+  },
+  passwordstyle: {
+    width: "220px",
+    textAlign: "justify",
+    height: "5px",
+    color: "#D43333",
+    fontSize: "13px",
+    margin: "0 10px",
+  },
+  cardContainer: {
+    width: "100%",
+  },
+  logindetailscontainer: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems : 'center',
+},
+cardbox: {
+    display: "flex",
+    justifyContent: {xs:"center" ,sm : 'center', md : 'flex-start'},
+    width: "90%",
+    padding: "10px",
+    flexWrap: "wrap",
+  },
+  heading: {
+    textAlign: "center",
+    fontSize: "30px",
+    fontWeight: "600",
+  },
+  card: {
+    boxShadow:
+      "rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px",
+    margin: "8px",
+    padding: "10px",
+    borderRadius : '10px'
+  },
+};
